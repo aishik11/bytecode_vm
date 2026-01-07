5ab4d537b716afd2bb2e2ee7a2ad51d4bd3db8d5
emgixiii <emgi@live.in>
2026-01-07 22:00:15 +0530

    feat: import opcodes as in assembler

---
+++ src/op_codes.hpp
@@ -0,0 +1,37 @@
+#ifndef OPCODE_H
+#define OPCODE_H
+
+typedef enum Opcode {
+  // Blank
+  NOP = 0x00,
+  // Data
+  PUSH,
+  POP,
+  DUP,
+  // Arithmetic
+  ADD = 0x10,
+  SUB,
+  MUL,
+  DIV,
+  CMP,
+  // Bitwise
+  AND = 0x15,
+  OR,
+  XOR,
+  NOT,
+  SHL,
+  SHR,
+  // Control
+  JMP = 0x20,
+  JZ,
+  JNZ,
+  // Memory
+  STORE = 0x30,
+  LOAD,
+  CALL,
+  RET,
+  // Halt
+  HALT = 0xFF,
+} Opcode;
+
+#endif // !OPCODE_H
